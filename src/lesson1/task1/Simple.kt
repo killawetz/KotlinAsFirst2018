bce5207214d0179daefdd1cda5266874ff2b6d03
killawetz <vasiliev2.rv@edu.spbstu.ru>
2018-09-13 00:41:21 +0500

    Решены первые 4 задачи!

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -3,6 +3,14 @@ package lesson1.task1
 
 import kotlin.math.*
 
+// Константы
+const val seconds_in_minute = 60
+const val centimeters_in_sagenes = 213.36
+const val centimeters_in_arshin = 71.12
+const val centimeters_in_vershok = 4.445
+const val centimerers_in_metr = 100
+
+
 /**
  * Пример
  *
@@ -58,7 +66,7 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = ( hours * sqr(seconds_in_minute)) + ( minutes * seconds_in_minute) + seconds
 
 /**
  * Тривиальная
@@ -67,7 +75,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (( sagenes * centimeters_in_sagenes ) +
+        ( arshins * centimeters_in_arshin) + ( vershoks * centimeters_in_vershok)) / centimerers_in_metr
 
 /**
  * Тривиальная
@@ -75,7 +84,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double = ( grad + min/ seconds_in_minute + sec/sqr(seconds_in_minute) ) * PI / 180
 
 /**
  * Тривиальная
@@ -83,7 +92,13 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double
+{
+    val Expression1 = x2-x1
+    val Expression2 = y2-y1
+    val Radicand = sqr(Expression1) + sqr(Expression2)
+    return sqrt(Radicand)
+}
 
 /**
  * Простая
@@ -91,7 +106,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = number % 10
 
 /**
  * Простая
